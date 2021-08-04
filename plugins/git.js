a698f7495ad8599f627c6bf8f38bc2e0fea53c6f
ameenahd <87332985+Ameen-ahd@users.noreply.github.com>
2021-08-04 21:42:56 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/MZDCN2M.jpeg";
+  r_text[0] = "https://tinyurl.com/ykxwd25q";
     
     
     var i = Math.floor(1*Math.random())
