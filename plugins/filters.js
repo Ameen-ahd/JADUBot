f58013492b242f08d36a18bf0ea22eb9f2177194
ameenahd <87332985+Ameen-ahd@users.noreply.github.com>
2021-08-05 14:58:52 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -56,7 +56,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
         if (!!message.mention && message.mention[0] == '917034817401@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/Mention.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
         }
-const array = ['Hi','King','alive','Bgm','Hlo','Love','Pali','sad','Song']
+const array = ['Hi','King','alive','Bgm','Hlo','Love','Pali','sad','Song','Neymar']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
