70b5786b01973104c5d5d366a2b573e616255969
ameenahd <87332985+Ameen-ahd@users.noreply.github.com>
2021-08-18 17:48:17 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -17,7 +17,7 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦■□■□🧚 ᳆𝐀𝐌 𝐁𝐎𝐓🌸 🧚■□■□⛦
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦■□□🧚᳆𝐀𝐌 𝐁𝐎𝐓🌸🧚■□□⛦
 
 
 𝐇𝐞𝐥𝐥𝐨💓 𝐈 𝐚𝐦 𝐚 ~JADBot~ .
